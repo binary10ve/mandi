5ca3a6666763939e4a20c5a96b2a8fbecedf7bcb
binary10ve <sanil.m.naik@gmail.com>
2013-12-10 21:05:42 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mandi::Application.config.secret_key_base = '4075a753ce2abf379c7d9aa2463faecfef829719436e2d39b02ee7cfa09de402dd8dd7cf41fc870854faebd3fb5fd37eb666e8ede64defb64852018c1599aa6a'
